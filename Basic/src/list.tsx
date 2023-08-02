75f1baee9258af03627faa9c54fc61c3065e7fba
Ismael Pareja Ramirez <ismael@uptodown.com>
2023-08-02 19:34:01 +0200

    add input search

---
+++ Basic/src/list.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+interface MemberEntity {
+  id: string;
+  login: string;
+  avatar_url: string;
+}
+
+export const ListPage: React.FC = () => {
+  const [members, setMembers] = React.useState<MemberEntity[]>([]);
+  const [search, setSearch] = React.useState("lemoncode");
+
+  React.useEffect(() => {
+    handleSearch();
+  }, []);
+
+  const handleSearch = () => {
+    fetch(`https://api.github.com/orgs/${search}/members`)
+      .then((response) => response.json())
+      .then((json) => setMembers(json));
+  }
+
+  return (
+    <>
+      <h2>Hello from List page</h2>+{" "}
+      <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}/>
+      <button type="button" onClick={handleSearch}>Search</button>
+      <div className="list-user-list-container">
+        <span className="list-header">Avatar</span>
+        <span className="list-header">Id</span>
+        <span className="list-header">Name</span>
+        {members.map((member) => (
+          <>
+            <img src={member.avatar_url} />
+            <span>{member.id}</span>
+            <Link to={`/detail/${member.login}`}>{member.login}</Link>
+          </>
+        ))}
+      </div>
+      <Link to="/detail">Navigate to detail page</Link>
+    </>
+  );
+};
